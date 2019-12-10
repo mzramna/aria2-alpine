66f874eb799af3803e8eed90f0b06fc9f1aaaae6
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2019-12-10 17:30:07 -0300

    created move on complete file
    it will move any completed download into the download folder to the subfolder download_complete,this could be easly changed into the config folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,8 +31,41 @@ max-overall-upload-limit=1K
 dht-file-path=${CONFIG_FOLDER:-/config}/dht.dat
 dht-file-path6=${CONFIG_FOLDER:-/config}/dht6.dat
 save-cookies=${CONFIG_FOLDER:-/config}/cookies.dat
-
-
+--on-bt-download-complete=${CONFIG_FOLDER:-/config}/mvcompleted.sh
+--on-download-complete=${CONFIG_FOLDER:-/config}/mvcompleted.sh
 EOF
+
+cat > ${CONFIG_FOLDER:-/config}/aria2.conf <<EOF
+#!/bin/sh
+ 
+# $1 is gid.
+# $2 is the number of files.
+# $3 is the path of the first file.
+ 
+DOWNLOAD=${DOWNLOAD_DIR:-/downloads} # no trailing slash!
+COMPLETE=${DOWNLOAD_DIR:-/downloads}/download_complete # no trailing slash!
+LOG=/config/mvcompleted.log
+SRC=$3
+ 
+if [ "$2" == "0" ]; then
+  echo `date` "INFO  no file to move for" "$1". >> "$LOG"
+  exit 0
+fi
+ 
+while true; do
+  DIR=`dirname "$SRC"`
+  if [ "$DIR" == "$DOWNLOAD" ]; then
+    echo `date` "INFO " "$3" moved as "$SRC". >> "$LOG"
+    mv -fv "$SRC" "$COMPLETE" >> "$LOG" 2>&1
+    exit $?
+  elif [ "$DIR" == "/" -o "$DIR" == "." ]; then
+    echo `date` ERROR "$3" not under "$DOWNLOAD". >> "$LOG"
+    exit 1
+  else
+    SRC=$DIR
+  fi
+done
+EOF
+
 
 exec "$@"
