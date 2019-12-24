422146aa2213f549c912b775f7ab9ef80687d366
Miguel Magalhães Lopes <miguelzinhorama@gmail.com>
2019-12-24 09:15:44 -0300

    teste de remoção de erro de comando

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,18 +42,17 @@ LOG=${CONFIG_FOLDER:-/config}/mvcompleted.log
 SRC=\$3
  
 if [ "\$2" == "0" ]; then
-  echo `date` "INFO  no file to move for" "\$1". >> "\$LOG"
+  echo `\date` "INFO  no file to move for" "\$1". >> "\$LOG"
   exit 0
 fi
  
 while true; do
   DIR=`dirname "\$SRC"`
   if [ "\$DIR" == "\$DOWNLOAD" ]; then
-    echo `date` "INFO " "$3" moved as "\$SRC". >> "\$LOG"
     mv -fv "\$SRC" "\$COMPLETE" >> "\$LOG" 2>&1
     exit $?
   elif [ "\$DIR" == "/" -o "\$DIR" == "." ]; then
-    echo `date` ERROR "\$3" not under "\$DOWNLOAD". >> "\$LOG"
+    echo `\date` ERROR "\$3" not under "\$DOWNLOAD". >> "\$LOG"
     exit 1
   else
     SRC=\$DIR
